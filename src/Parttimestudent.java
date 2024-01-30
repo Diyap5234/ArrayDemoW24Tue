5b8ea66cce768eed3550c45a942268c23599bdba
Diya Patel <dpatel34922004@gmail.com>
2024-01-30 12:41:12 -0500

    addede a class

---
+++ src/Parttimestudent.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author dpate
+ */
+public class Parttimestudent extends student {
+    private int numofCourses;
+
+    public Parttimestudent(int numofCourses) {
+        this.numofCourses = numofCourses;
+    }
+
+   
+    
+}
